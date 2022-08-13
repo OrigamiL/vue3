9309e6712fd07e5e9c81c369efdbf252ce6cab86
OrigamiL <1714573172@qq.com>
2022-08-13 18:18:12 +0800

    8.13 commit

--- vue01/src/App.vue
+++ vue01/src/App.vue
@@ -4,7 +4,13 @@
     <p class="active">Hello</p>
 <!--  第二种，放置对象  -->
     <p :class="{active:isActive,negative:isNegative}">Hello</p>
-    <button @click="isActive=false"></button>
+    <button @click="isActive=!isActive;isNegative=!isNegative"></button>
+<!--  和普通的类同时存在，不会冲突  -->
+    <p class="active" :class="{negative:isNegative}">Hello</p>
+
+    <p :class="classObj">Hello</p>
+<!--  使用computed  -->
+    <p :class="classObject">Hello</p>
   </div>
 </template>
 
@@ -14,9 +20,23 @@ export default {
     return{
       message:"helloWorld",
       isActive:true,
-      isNegative:true,
+      isNegative:false,
+      error: {},
+      classObj:{
+        active:true,
+        negative:false,
+      }
     };
   },
+  computed:{
+    //方法简写: a:function(){}可以简写为a(){}
+    classObject: function () {
+      return {
+        active:this.isActive && !this.error,
+        negative:this.error
+      }
+    },
+  }
 };
 </script>
 
@@ -24,9 +44,11 @@ export default {
   .active{
     font-size: 50px;
     color: #1a1a1a;
+    font-style: oblique;
   }
-  p{
-    font-size: 50px;
+  .negative{
+    font-size: 60px;
     color: #66ccff;
+    font-weight: bold;
   }
 </style>
