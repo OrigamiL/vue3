f1ecc30c03fcc285b23ca108026f006b3c4f9145
OrigamiL <1714573172@qq.com>
2022-08-20 18:20:26 +0800

    8.20 commit

--- vue01/src/App.vue
+++ vue01/src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div>
-    <Content :message="message"></Content>
+    <Content class="myContent" id="content" :message="message" @injectCounter="getCounter"></Content>
+    <Content ref="content" @injectCounter="getCounter"></Content>
     <button @click="message='???'">按钮</button>
+    <h2>---{{counter}}</h2>
   </div>
 </template>
 
@@ -13,7 +15,18 @@ export default {
   data() {
     return {
       message:"HelloWorld",
+      counter:0,
     }
+  },
+  methods:{
+    getCounter(value){
+      this.counter=value;
+      console.log(value)
+    }
+  },
+  mounted() {
+    console.log("打印"+this.$refs.content.counter);
+    this.$refs.content.sendParent();
   }
 }
 </script>
--- vue01/src/components/Content.vue
+++ vue01/src/components/Content.vue
@@ -1,14 +1,19 @@
 <template>
   <div>
     <h2>{{message}}</h2>
+    <button @click="sendParent">发送数据</button>
   </div>
 </template>
 
 <script>
+import {h, onUpdated, ref, toRefs, watchEffect} from "vue";
+
 export default {
   name: "Content",
   data() {
-    return {}
+    return {
+      name:"张",
+    }
   },
   props:{
     message:{
@@ -17,9 +22,39 @@ export default {
       required:false,
     }
   },
-  setup(props){
+  setup(props,context){
     console.log(props)
-    console.log(props.message);
+    /*watchEffect(()=>{
+      console.log(props.message)
+    })*/
+    const {message} = toRefs(props) //解构需要使用toRefs让其保持响应式
+    onUpdated(()=>{
+      console.log("message:"+message.value)
+    });
+    //Attribute(非响应式对象，等同于$attrs)   App中使用了<Content/>组件，其中定义的class  id 都可以通过attrs拿到
+    console.log(context.attrs)
+    //插槽（非响应式对象，等同于$slots）
+    //console.log(context.slots)
+    //触发事件(方法，等同于$emit)
+    console.log(context.emit)
+    const counter = ref(20);
+    function sendParent() {
+      context.emit("injectCounter",counter.value)
+    }
+
+    //暴露公共property（函数）
+    //console.log(context.expose)
+/*    return{
+      sendParent,
+    }*/
+    context.expose({
+      sendParent,counter
+    })
+    return ()=>h('div',counter.value)
+
+  },
+  mounted() {
+    console.log(this.message)
   }
 }
 </script>
